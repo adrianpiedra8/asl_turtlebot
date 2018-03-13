0339fa7eaf0c5711a4b49316486a324b0b06042d
Greg Katz <gkatz@stanford.edu>
2018-03-12 19:26:26 -0700

    Add landmarks

---
+++ scripts/landmarks.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+def findmatch(existing, new, thresh):
+    dist = np.linalg.norm(existing - new)
+    if np.any(dist < thresh):
+        return np.where(dist < thresh)[0][0]
+    return None
+
+class StopSigns:
+    def __init__(self, dist_thresh=1):
+        self.locations = np.zeros((0, 2))
+        self.observations_count = []
+        self.dist_thresh = dist_thresh
+
+    def add_observation(self, observation):
+        existing = findmatch(self.locations, observation)
+        if existing != None:
+            self.update_location(existing, observation)
+        else:
+            self.locations = np.vstack((self.locations, observation))
+            self.observations_count.append(1)
+
+    def update_location(self, index, observation):
+        prev = self.locations[index]
+        n = self.observations_count[index]
+
+        # Average of all observations we have seen so far
+        self.locations[index] = (prev*n + observation)/(n+1)
+
+        # Increment observations count
+        self.observations_count[index] = n + 1
+
+class AnimalWaypoints:
+    def __init__(self, dist_thresh=1):
+        self.poses = np.zeros((0, 3))
+        self.locations = np.zeros((0,2))
+        self.dist_thresh = dist_thresh
+        self.bbox_heights = np.zeros((0))
+        
+    def update_location(self, index, observation):
+        prev = self.locations[index]
+        n = self.observations_count[index]
+
+        # Average of all observations we have seen so far
+        self.locations[index] = (prev*n + observation)/(n+1)
+
+        # Increment observations count
+        self.observations_count[index] = n + 1
+
+    def add_observation(self, observation, pose, bbox_height):
+        existing = findmatch(self.locations, observation)
+        if existing != None:
+            self.update_location(existing, observation, pose)
+        else:
+            self.locations = np.vstack((self.locations, observation))
+            self.poses = np.vstack((self.poses, pose))
+            self.bbox_heights = np.append(self.bbox_heights, bbox_height)
+
+    def update_location(self, index, observation, pose, bbox_height):
+
+        # Only update if new observation has a bigger bounding box
+        if bbox_height > self.bbox_heights[index]:
+            self.locations[index] = observation
+            self.poses[index] = pose
+            self.bbox_heights[index] = bbox_height
